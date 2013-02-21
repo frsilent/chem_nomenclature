718cf2efddd196634f7d03e8e537ab602dc60e81
Roland Heintze <rheintze@linux.com>
2013-02-21 18:23:01 -0500

    Initializing Python port

---
+++ Alkane.py
@@ -0,0 +1,12 @@
+__author__ = 'rheintze'
+
+
+class Alkane:
+    """
+    A class used to implement an organic compound.
+    """
+
+    def init(self, longestChain, noSub):
+        #integers longestChain, maxSubLength, & subPosition[]
+        self.longestChain = longestChain
+        pass
