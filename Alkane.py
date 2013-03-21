d0c99dae5500e290cdcff194f553f95f3a84a09c
Roland <frsilentwolf@gmail.com>
2013-03-21 16:28:58 -0400

    Removed the Java Processing files; Development will continue in Python. Also removed testing file

--- Alkane.py
+++ Alkane.py
@@ -6,7 +6,14 @@ class Alkane:
     A class used to implement an organic compound.
     """
 
+    chain = ('methane','ethane','propane','butane','pentane','hexane','heptane','octane','nonane','decane')
+    substituent = ('methyl','ethyl','propyl','butyl','pentyl')
     def init(self, longestChain, noSub):
         #integers longestChain, maxSubLength, & subPosition[]
         self.longestChain = longestChain
         pass
+
+if __name__ == '__main__':
+    #testing
+    a = Alkane()
+    pass
