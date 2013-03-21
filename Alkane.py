d8bbde5a2b2aeca2612681b1ace47356c5ae3ef3
Roland <frsilentwolf@gmail.com>
2013-03-21 19:08:53 -0400

    Added wrapper for http://www.chemspider.com/ API

--- Alkane.py
+++ Alkane.py
@@ -1,16 +1,16 @@
 __author__ = 'rheintze'
 
+import chemspipy
 
-class Alkane:
+class Alkane():
     """
     A class used to implement an organic compound.
     """
 
     chain = ('methane','ethane','propane','butane','pentane','hexane','heptane','octane','nonane','decane')
     substituent = ('methyl','ethyl','propyl','butyl','pentyl')
-    def init(self, longestChain, noSub):
+    def init(self):
         #integers longestChain, maxSubLength, & subPosition[]
-        self.longestChain = longestChain
         pass
 
 if __name__ == '__main__':
