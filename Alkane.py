00eaba72931fdc5f7e77a95acb58e6c27da59c1a
Roland <frsilentwolf@gmail.com>
2013-03-21 21:40:15 -0400

    Added wrapper for http://www.chemspider.com/ API. Now working on pulling proper nomenclatures from chemspider

--- Alkane.py
+++ Alkane.py
@@ -1,19 +1,32 @@
 __author__ = 'rheintze'
 
-import chemspipy
+from chemspipy import *
 
-class Alkane():
+class Alkane:
     """
     A class used to implement an organic compound.
     """
 
     chain = ('methane','ethane','propane','butane','pentane','hexane','heptane','octane','nonane','decane')
     substituent = ('methyl','ethyl','propyl','butyl','pentyl')
-    def init(self):
-        #integers longestChain, maxSubLength, & subPosition[]
+
+    def __init__(self, smiles):
+        self.smiles = smiles #simplified molecular-input line-entry system, an ascii representation of a molecular graph
+        print(smiles)
+        #modify smiles to match correct query
+        compound_maybe = find_one(smiles)
+        print(compound_maybe.commonname)
+        print(compound_maybe.smiles)
+
+        #once a compound is identified it can be pulled from chemspider
+        #then initialized as a compound using Compound(id)
+        #x = Compound(236)
+        #print(x.imageurl)
+
         pass
 
 if __name__ == '__main__':
     #testing
-    a = Alkane()
+    a = Alkane('CC(C)CC')           #CC(C)CC 2-Methylbutane
+    b = Alkane('CC(C)(C)C')         #CC(C)(C)C 2,2-Dimethylpropane
     pass
