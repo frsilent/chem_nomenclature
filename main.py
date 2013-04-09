ea5b03f217173b5e301429dfdb2e3cbf1fffb4b1
Roland <rheintze@linux.com>
2013-04-09 12:00:32 -0400

    trivial commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ __author__ = 'rheintze'
 
 import sys
 from PyQt4 import QtCore, QtGui
+from PyQt4.QtGui import QImage
 from ui import Ui_MainWindow #pulls from ui.py which can be created from .ui xml file using the pyuic tool
 
 from Alkane import Alkane
@@ -12,7 +13,7 @@ class StartQT4(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self,parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.view = View(self.ui.graphicsView)
+        self.view = DrawView(self.ui.graphicsView)
 
         self.target = None
 
@@ -27,31 +28,89 @@ class StartQT4(QtGui.QMainWindow):
     def makeCarbon(self):
         pass
 
-class View(QtGui.QGraphicsView):
+class DrawView(QtGui.QGraphicsView):
     def __init__(self,parent):
-        QtGui.QGraphicsView.__init__(self,parent)
+        QtGui.QWidget.__init__(self,parent)
+        self.width = 600
+        self.height = 300
+        
+        self.num_rows = 15
+        self.num_cols = 30
+        
+        #Size of each square
+        self.col_width = self.width / self.num_cols
+        self.row_height = self.height / self.num_rows
+        
+        #2D Array to remember which squares have carbons in them
+        self.carbonBooleanMatrix = []
+        for row in range(0, self.num_rows):
+            self.carbonBooleanMatrix.append([])
+            for col in range(0, self.num_cols):
+                self.carbonBooleanMatrix[row].append(False)
+        
+        self.setScene(QtGui.QGraphicsScene(self.parent()))
+        self.scene = self.scene()
+        self.scene.setSceneRect(0,0,600,300)
 
-        self.setScene(QtGui.QGraphicsScene(self))
-        # Alternatively, would be good to find a way to inherit parent.size()
-        self.setSceneRect(QtCore.QRectF(0,0,parent.width(),parent.height())) #Yes this is bad and I feel bad
-
-        self.pix = QtGui.QPixmap('carbon.png')
-
+        #Load and resize image to fit squarely in the grid
+        self.carbon_image = QtGui.QPixmap('carbon.png').scaled(self.col_width, self.row_height)
+        self.addGridToScene()
+        print(self.scene.width(), self.scene.height())
+        
+    def addGridToScene(self):
+        #Set up pen
+        pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
+        #Draw Horizontal Lines
+        for y in range(0, self.num_rows-1): # don't draw the two outside edge lines
+            self.scene.addLine(0, (y+1)*self.row_height, self.width, (y+1)*self.row_height, pen)
+            if y == self.num_rows-2:
+                print((y+1)*self.row_height)
+        #Draw Vertical Lines
+        for x in range(0, self.num_cols-1): # don't draw the two outside edge lines
+            self.scene.addLine((x+1)*self.col_width, 0, (x+1)*self.col_width, self.height, pen)
+            if x == self.num_cols-2:
+                print((x+1)*self.col_width)
+            
+    def drawCarbons(self, qp):
+        for row in range(0, self.num_rows):
+            for col in range(0, self.num_cols):
+                #Is there a carbon at this logical position to draw?
+                if self.carbonBooleanMatrix[row][col]:
+                    #Get the transformed graphical coordinates
+                    coords = logicalToScreen(self, (row, col))
+                    #Draw the carbon image at 
+                    qp.drawImage(coords[0], coords[1], self.carbon_image)
+    
+    #Add or remove carbons based on click position and mouse button
     def mousePressEvent(self,event):
-        if event.button() == QtCore.Qt.LeftButton:
-            self.position = self.mapToScene(event.pos())
-            print('left-clicked at (%d, %d)' % (self.position.x(), self.position.y()))
-
-            self.scene().addPixmap(self.pix).setPos(self.position)
-
-        elif event.button() == QtCore.Qt.RightButton:
-            self.position = self.mapToScene(event.pos())
-            print('right-clicked at (%d, %d)' % (self.position.x(), self.position.y()))
-
-            self.scene().removeItem(self.scene().itemAt(self.position))
-
-    def mouseReleaseEvent(self,event):
-        pass
+        #Convert global QT coordinates to logical coordinates
+        x, y = self.screenToLogical(event.pos())
+        print("Logical",x,y)
+        print("Screen",event.pos().x(),event.pos().y())
+        #Left click to add a carbon
+#        if event.button() == QtCore.Qt.LeftButton:
+#            #Is this square empty?
+#            if not self.carbonBooleanMatrix[x][y]:
+#                #Place a carbon in this square
+#                # self.carbonBooleanMatrix[x][y]=True
+#        #Right click to remove a carbon
+#        elif event.button() == QtCore.Qt.RightButton:
+#            #Is this square filled?
+#            if self.carbonBooleanMatrix[x][y]:
+#                #Remove the carbon from this square
+#                self.carbonBooleanMatrix[x][y]=False
+#                pass
+    
+    #Return (x,y) in logical carbonBooleanMatrix coordinates    
+    def screenToLogical(self, position):
+        return (int(position.x()/self.col_width), int(position.y()/self.row_height))
+    
+    #Return (x,y) in top-left corner screen coordinates
+    def logicalToScreen(self, tuple):
+        return (int(position.x()*self.col_width), int(position.y()*self.row_height))
+    
+#    def mouseReleaseEvent(self,event):
+#        pass
 
 
 if __name__ == '__main__':
--- ui.py
+++ ui.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'launch.ui'
 #
-# Created: Sun Apr  7 16:10:36 2013
-#      by: PyQt4 UI code generator 4.10
+# Created: Sun Apr 14 20:52:55 2013
+#      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,32 +12,20 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+    _fromUtf8 = lambda s: s
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(1165, 781)
+        MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.graphicsView = QtGui.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(10, 190, 871, 531))
-        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
         self.chemSpiderView = QtWebKit.QWebView(self.centralwidget)
-        self.chemSpiderView.setGeometry(QtCore.QRect(910, 430, 231, 291))
-        self.chemSpiderView.setUrl(QtCore.QUrl(_fromUtf8("about:blank")))
+        self.chemSpiderView.setGeometry(QtCore.QRect(610, 150, 181, 401))
+        self.chemSpiderView.setProperty("url", QtCore.QUrl(_fromUtf8("about:blank")))
         self.chemSpiderView.setObjectName(_fromUtf8("chemSpiderView"))
         self.chemSpiderLabel = QtGui.QLabel(self.centralwidget)
-        self.chemSpiderLabel.setGeometry(QtCore.QRect(910, 390, 101, 17))
+        self.chemSpiderLabel.setGeometry(QtCore.QRect(670, 100, 101, 17))
         self.chemSpiderLabel.setObjectName(_fromUtf8("chemSpiderLabel"))
         self.randomButton = QtGui.QPushButton(self.centralwidget)
         self.randomButton.setGeometry(QtCore.QRect(30, 20, 151, 91))
@@ -55,7 +43,7 @@ class Ui_MainWindow(object):
         self.inchiBox.setGeometry(QtCore.QRect(280, 150, 141, 31))
         self.inchiBox.setObjectName(_fromUtf8("inchiBox"))
         self.animateButton = QtGui.QPushButton(self.centralwidget)
-        self.animateButton.setGeometry(QtCore.QRect(940, 280, 201, 71))
+        self.animateButton.setGeometry(QtCore.QRect(540, 10, 201, 71))
         self.animateButton.setObjectName(_fromUtf8("animateButton"))
         self.nomenclatureLabel = QtGui.QLabel(self.centralwidget)
         self.nomenclatureLabel.setGeometry(QtCore.QRect(210, 20, 111, 17))
@@ -73,9 +61,26 @@ class Ui_MainWindow(object):
         self.generateButton = QtGui.QPushButton(self.centralwidget)
         self.generateButton.setGeometry(QtCore.QRect(440, 150, 87, 27))
         self.generateButton.setObjectName(_fromUtf8("generateButton"))
+        self.graphicsView = QtGui.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(20, 200, 625, 325))
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.graphicsView.sizePolicy().hasHeightForWidth())
+        self.graphicsView.setSizePolicy(sizePolicy)
+        self.graphicsView.setMinimumSize(QtCore.QSize(625, 325))
+        self.graphicsView.setMaximumSize(QtCore.QSize(625, 325))
+        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        self.graphicsView.setBackgroundBrush(brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        self.graphicsView.setForegroundBrush(brush)
+        self.graphicsView.setSceneRect(QtCore.QRectF(0.0, 0.0, 600.0, 300.0))
+        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1165, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
@@ -86,14 +91,14 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Chem Wizard", None))
-        self.chemSpiderLabel.setText(_translate("MainWindow", "Additional Info:", None))
-        self.randomButton.setText(_translate("MainWindow", "Random", None))
-        self.smilesLabel.setText(_translate("MainWindow", "SMILES:", None))
-        self.inchiLabel.setText(_translate("MainWindow", "InChi", None))
-        self.animateButton.setText(_translate("MainWindow", "Animate", None))
-        self.nomenclatureLabel.setText(_translate("MainWindow", "Nomenclature:", None))
-        self.checkButton.setText(_translate("MainWindow", "Check!", None))
-        self.generateButton.setText(_translate("MainWindow", "Generate", None))
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Chem Wizard", None, QtGui.QApplication.UnicodeUTF8))
+        self.chemSpiderLabel.setText(QtGui.QApplication.translate("MainWindow", "Additional Info:", None, QtGui.QApplication.UnicodeUTF8))
+        self.randomButton.setText(QtGui.QApplication.translate("MainWindow", "Random", None, QtGui.QApplication.UnicodeUTF8))
+        self.smilesLabel.setText(QtGui.QApplication.translate("MainWindow", "SMILES:", None, QtGui.QApplication.UnicodeUTF8))
+        self.inchiLabel.setText(QtGui.QApplication.translate("MainWindow", "InChi", None, QtGui.QApplication.UnicodeUTF8))
+        self.animateButton.setText(QtGui.QApplication.translate("MainWindow", "Animate", None, QtGui.QApplication.UnicodeUTF8))
+        self.nomenclatureLabel.setText(QtGui.QApplication.translate("MainWindow", "Nomenclature:", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkButton.setText(QtGui.QApplication.translate("MainWindow", "Check!", None, QtGui.QApplication.UnicodeUTF8))
+        self.generateButton.setText(QtGui.QApplication.translate("MainWindow", "Generate", None, QtGui.QApplication.UnicodeUTF8))
 
 from PyQt4 import QtWebKit
