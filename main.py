277491250c889b8236d2e8cd3735c9c8e070b99b
Roland <frsilentwolf@gmail.com>
2013-04-08 22:23:10 -0400

    Learning the mouse events; need to figure out exactly how this framework handles scenes and views

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class StartQT4(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self,parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.view = View(self.ui.graphicsView)
         self.target = None
 
         QtCore.QObject.connect(self.ui.generateButton,QtCore.SIGNAL("clicked()"),self.generate)
@@ -20,12 +21,31 @@ class StartQT4(QtGui.QMainWindow):
         #target = Alkane(self.ui.inchiBox.toPlainText())
         print(str(self.ui.inchiBox.toPlainText()))
     def makeCarbon(self):
-        print("graphicsView clicked in")
-
-    def draw(self):
         pass
 
+class View(QtGui.QGraphicsView):
+    def __init__(self,parent):
+        QtGui.QGraphicsView.__init__(self,parent)
+        #TODO: This needs to be restructed so that the scene inherits the view from the graphicsview object
+        self.setScene(QtGui.QGraphicsScene(self)) #Particularly this line needs to be sized right
+        print(self.viewport().rect())
+        #self.setSceneRect(QtGui.QGraphicsView.sceneRect())
+        self.setSceneRect(QtCore.QRectF(self.viewport().rect())) #this adds box for me to draw in; kewl
+
+    def mousePressEvent(self,event):
+        self._start = event.pos()
+    def mouseReleaseEvent(self,event):
+        start = QtCore.QPointF(self.mapToScene(self._start))
+        end = QtCore.QPointF(self.mapToScene(event.pos()))
+        self.scene().addItem(QtGui.QGraphicsLineItem(QtCore.QLineF(start,end)))
+        for point in (start,end):
+            text = self.scene().addSimpleText('(%d, %d)' % (point.x(), point.y()))
+            text.setBrush(QtCore.Qt.red)
+            text.setPos(point)
+
+
 if __name__ == '__main__':
+    import sys
     app = QtGui.QApplication(sys.argv)
     myapp = StartQT4()
     myapp.target = Alkane('CC')
