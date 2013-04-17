ec014bb49e8c5ec0fa0be7e590525b6da3a4902c
tim <tim@tim-dv6-xubuntu.(none)>
2013-04-16 23:33:09 -0400

    Adds PyDev pycache and *.pyc files to .gitignore

---
+++ Carbon.py
@@ -0,0 +1,9 @@
+class Carbon:
+    """
+    A class used to represent a carbon in an alkane molecule
+    """
+    def __init__(self):
+        self.north = self.south = self.east = self.west = None
+    
+    
+        
